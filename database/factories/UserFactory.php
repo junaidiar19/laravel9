c05e792c87bf0c1e3fa93253cf7d88528580a97a
Junaidi Abdul Rahman <junaidiar.fcb@gmail.com>
2022-03-04 14:45:48 +0800

    remove materi

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,6 +17,7 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        // avatar from unsplash
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
